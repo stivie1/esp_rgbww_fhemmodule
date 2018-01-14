4ccbaea8f5e6b93084b5e3985506275b9e05b4fb
verybadsoldier <vbs@springrts.de>
2018-01-14 12:37:31 +0100

    added reading info-webapp_version

--- FHEM/32_EspLedController.pm
+++ FHEM/32_EspLedController.pm
@@ -738,19 +738,21 @@ sub EspLedController_ParseInfo(@) {
     else {
       fhem( "deletereading " . $hash->{NAME} . " info-.*", 1 );
       readingsBeginUpdate($hash);
-      readingsBulkUpdate( $hash, 'info-deviceid', $res->{deviceid} );
-      readingsBulkUpdate( $hash, 'info-firmware', $res->{git_version} );
-      readingsBulkUpdate( $hash, 'info-event_num_clients', $res->{event_num_clients} );
-      readingsBulkUpdate( $hash, 'info-sming_version', $res->{sming} );
-      readingsBulkUpdate( $hash, 'info-current_rom_slot', $res->{current_rom} );
-      readingsBulkUpdate( $hash, 'info-uptime', $res->{uptime} );
+      readingsBulkUpdate( $hash, 'info-deviceid',               $res->{deviceid} );
+      readingsBulkUpdate( $hash, 'info-firmware',               $res->{git_version} );
+      readingsBulkUpdate( $hash, 'info-sming_version',          $res->{sming} );
+      readingsBulkUpdate( $hash, 'info-webapp_version',         $res->{webapp_version} );
 
-      readingsBulkUpdate( $hash, 'info-connection-ssid',       $res->{connection}->{ssid} );
-      readingsBulkUpdate( $hash, 'info-connection-dhcp',       $res->{connection}->{dhcp} );
-      readingsBulkUpdate( $hash, 'info-connection-ip_address', $res->{connection}->{ip} );
-      readingsBulkUpdate( $hash, 'info-connection-netmask',    $res->{connection}->{netmask} );
-      readingsBulkUpdate( $hash, 'info-connection-gateway',    $res->{connection}->{gateway} );
-      readingsBulkUpdate( $hash, 'info-connection-mac',        $res->{connection}->{mac} );
+      readingsBulkUpdate( $hash, 'info-event_num_clients',      $res->{event_num_clients} );
+      readingsBulkUpdate( $hash, 'info-current_rom_slot',       $res->{current_rom} );
+      readingsBulkUpdate( $hash, 'info-uptime',                 $res->{uptime} );
+
+      readingsBulkUpdate( $hash, 'info-connection-ssid',        $res->{connection}->{ssid} );
+      readingsBulkUpdate( $hash, 'info-connection-dhcp',        $res->{connection}->{dhcp} );
+      readingsBulkUpdate( $hash, 'info-connection-ip_address',  $res->{connection}->{ip} );
+      readingsBulkUpdate( $hash, 'info-connection-netmask',     $res->{connection}->{netmask} );
+      readingsBulkUpdate( $hash, 'info-connection-gateway',     $res->{connection}->{gateway} );
+      readingsBulkUpdate( $hash, 'info-connection-mac',         $res->{connection}->{mac} );
       
       readingsEndUpdate( $hash, 1 );
     }
